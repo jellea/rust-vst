4caaf629a8236a8e44f68faca9470d5f9424d658
Travis CI User <travis@example.org>
2018-01-12 04:15:28 +0000

    Update documentation

---
+++ implementors/core/iter/traits/trait.IntoIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["vst"] = ["impl&lt;'a, T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"vst/buffer/struct.Inputs.html\" title=\"struct vst::buffer::Inputs\">Inputs</a>&lt;'a, T&gt;","impl&lt;'a, T:&nbsp;<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/trait.IntoIterator.html\" title=\"trait core::iter::traits::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"vst/buffer/struct.Outputs.html\" title=\"struct vst::buffer::Outputs\">Outputs</a>&lt;'a, T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/arith/trait.Sub.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["vst"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"vst/api/flags/struct.Channel.html\" title=\"struct vst::api::flags::Channel\">Channel</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"vst/api/flags/struct.Plugin.html\" title=\"struct vst::api::flags::Plugin\">Plugin</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"vst/api/flags/struct.ModifierKey.html\" title=\"struct vst::api::flags::ModifierKey\">ModifierKey</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.Sub.html\" title=\"trait core::ops::arith::Sub\">Sub</a> for <a class=\"struct\" href=\"vst/api/flags/struct.MidiEvent.html\" title=\"struct vst::api::flags::MidiEvent\">MidiEvent</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
